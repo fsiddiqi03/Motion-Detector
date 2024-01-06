b3d4e3bcd65d81761a7d27a1bffbb22ae60f132e
Faris Siddiqi <farisiddiqi@gmail.com>
2024-01-05 23:08:39 -0600

    bug fixes

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 from gpiozero import MotionSensor
 from hue_api import hueAPI
 
-
 hue = hueAPI()
-
-
 pir = MotionSensor(17)
 
+
+
+
 while True:
-	pir.wait_for_motion()
-	print("You moved")
-	if not hue.lightStatus():
-	    hue.turnON()
+    pir.wait_for_motion()
+    print("You moved")
+    if not hue.lightStatus():
+        hue.turnON()
+        print('light turned on')
+    else:
+        print('light already on')
+
+
+
+
+
