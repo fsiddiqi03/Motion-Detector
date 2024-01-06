83e9d723daf013e382868814cd99ca530c55fb64
Faris Siddiqi <farisiddiqi@gmail.com>
2024-01-05 23:03:16 -0600

    initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from gpiozero import MotionSensor
+from hue_api import hueAPI
+
+
+hue = hueAPI()
+
+
+pir = MotionSensor(17)
+
+while True:
+	pir.wait_for_motion()
+	print("You moved")
+	if not hue.lightStatus():
+	    hue.turnON()
